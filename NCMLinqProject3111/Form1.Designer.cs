f95ff75e5018cdd6e5718aa870fc26065c1af37a
hyunjin <s2113@e-mirim.hs.kr>
2023-08-10 14:07:00 +0900

    초기 커밋.

---
+++ NCMLinqProject3111/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NCMLinqProject3111
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.animalBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.healthyDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animalBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.typesDataGridViewTextBoxColumn,
+            this.healthyDataGridViewCheckBoxColumn});
+            this.dataGridView1.DataSource = this.animalBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 291);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 129);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "전체 목록";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 129);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "이름 정렬";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(334, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 129);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "나이 정렬";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(494, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 129);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "종 정렬";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(658, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 129);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "아픈 동물 보기";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // animalBindingSource
+            // 
+            this.animalBindingSource.DataSource = typeof(NCMLinqProject3111.Animal);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "Age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "Age";
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            // 
+            // typesDataGridViewTextBoxColumn
+            // 
+            this.typesDataGridViewTextBoxColumn.DataPropertyName = "Types";
+            this.typesDataGridViewTextBoxColumn.HeaderText = "Types";
+            this.typesDataGridViewTextBoxColumn.Name = "typesDataGridViewTextBoxColumn";
+            // 
+            // healthyDataGridViewCheckBoxColumn
+            // 
+            this.healthyDataGridViewCheckBoxColumn.DataPropertyName = "Healthy";
+            this.healthyDataGridViewCheckBoxColumn.HeaderText = "Healthy";
+            this.healthyDataGridViewCheckBoxColumn.Name = "healthyDataGridViewCheckBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animalBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn healthyDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.BindingSource animalBindingSource;
+    }
+}
+
---
+++ NCMLinqProject3111/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NCMLinqProject3111
+{
+    public partial class Form1 : Form
+    {
+        static private List<Animal> animals = new List<Animal>()
+        {
+            new Animal(){Name = "봄이", Age =5, Types = "고양이", Healthy = true},
+            new Animal(){Name = "아리", Age = 2, Types = "고양이", Healthy = true },
+            new Animal(){Name = "콩이", Age = 7, Types = "강아지", Healthy = false },
+            new Animal(){Name = "푸바오", Age = 3, Types = "판다", Healthy = true },
+            new Animal(){Name = "아르세우스", Age =10, Types = "알파카", Healthy = false },
+            new Animal(){Name = "피카츄", Age =7, Types = "쥐", Healthy = true },
+            new Animal(){Name = "똘이", Age = 12, Types = "강아지", Healthy = false},
+            new Animal(){Name = "랑호", Age = 7, Types = "호랑이", Healthy = false },
+            new Animal(){Name = "현진이", Age = 8, Types = "토끼", Healthy = true },
+            new Animal(){Name = "필릭스", Age = 2, Types = "병아리", Healthy =false },
+            new Animal(){Name = "월이", Age = 5, Types = "호랑이", Healthy = false },
+            new Animal(){Name = "설이", Age = 7, Types = "표범", Healthy = true },
+            new Animal(){Name = "영이", Age = 9, Types = "앵무새", Healthy = false },
+            new Animal(){Name = "자리", Age = 1, Types = "거북이", Healthy = false },
+            new Animal(){Name = "하리", Age = 2, Types = "부엉이", Healthy = true},
+            new Animal(){Name = "꼬부기", Age = 15, Types = "거북이", Healthy = false },
+            new Animal(){Name = "돌돌이", Age = 17, Types = "돌고래", Healthy = true },
+            new Animal(){Name = "이름이", Age = 9, Types = "쥐", Healthy = true },
+            new Animal(){Name = "무명이", Age = 2, Types = "사슴", Healthy =true },
+            new Animal(){Name = "레오나르도빚갚으리오", Age = 18, Types = "곰", Healthy = false },
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            animalBindingSource.DataSource = animals;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            animalBindingSource.DataSource = from item in animals
+                                             orderby item.Name descending
+                                             select item;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            animalBindingSource.DataSource = from item in animals
+                                             orderby item.Age descending
+                                             select item;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            animalBindingSource.DataSource = from item in animals
+                                             orderby item.Types descending
+                                             select item;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            animalBindingSource.DataSource = from item in animals
+                                             where item.Healthy == false
+                                             orderby item.Types descending
+                                             select item;
+        }
+    }
+}
